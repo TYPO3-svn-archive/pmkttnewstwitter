bc83761f30c0217386b3627f257f3ee6d13a0100
pmk65 <pmk65@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-11 18:50:33 +0000

    Added function to create a link back to the singleView page on site. Fixed problems with User TSConfig not working.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pmkttnewstwitter/trunk@26425 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_pmkttnewstwitter.php
+++ class.tx_pmkttnewstwitter.php
@@ -21,39 +21,51 @@
 	*
 	*  This copyright notice MUST APPEAR in all copies of the script!
 	***************************************************************/
-	 
+
 	/**
-	* [CLASS/FUNCTION INDEX of SCRIPT]
-	*
-	*
-	*
-	*   44: class tx_pmkttnewstwitter
-	*   56:     function processDatamap_afterDatabaseOperations($status, $table, $id, &$fieldArray, &$reference)
-	*   80:     function twit($twitter_data)
-	*  104:     function getConfig($pageId)
-	*
-	* TOTAL FUNCTIONS: 3
-	* (This index is automatically created/updated by the extension "extdeveval")
-	*
-	*/
-	 
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ *
+ *
+ *   55: class tx_pmkttnewstwitter
+ *   67:     function processDatamap_afterDatabaseOperations($status, $table, $id, &$fieldArray, &$reference)
+ *  100:     function twit($twitter_data)
+ *  125:     function makeSingleLink()
+ *  147:     function init_tmpl($pageId,$template_uid=0)
+ *  167:     function getNewsCategory($uid)
+ *  188:     function createTinyUrl($longURL)
+ *  199:     function getConfig($pageId)
+ *
+ * TOTAL FUNCTIONS: 7
+ * (This index is automatically created/updated by the extension "extdeveval")
+ *
+ */
+
+/* Classes required for creating a tmpl object */
+require_once(PATH_t3lib."class.t3lib_extobjbase.php");
+require_once(PATH_t3lib."class.t3lib_tsparser_ext.php");
+require_once(PATH_t3lib."class.t3lib_page.php");
+
+require_once(t3lib_extMgm::extPath('pagepath').'class.tx_pagepath_api.php');
+
 	/**
-	* Class for posting tt_news entries on twitter.
-	*
-	*/
+	 * Class for posting tt_news entries on twitter.
+	 *
+	 */
 	class tx_pmkttnewstwitter {
-		 
+
 		/**
-		* Main function. Hook from t3lib/class.t3lib_tcemain.php
-		*
-		* @param string  $status: Status of the current operation, 'new' or 'update
-		* @param string  $table: The table currently processing data for
-		* @param string  $id: The record uid currently processing data for, [integer] or [string] (like 'NEW...')
-		* @param array  $fieldArray: The field array of a record
-		* @param object  $reference: reference to parent object
-		* @return void
-		*/
+		 * Main function. Hook from t3lib/class.t3lib_tcemain.php
+		 *
+		 * @param	string		$status: Status of the current operation, 'new' or 'update
+		 * @param	string		$table: The table currently processing data for
+		 * @param	string		$id: The record uid currently processing data for, [integer] or [string] (like 'NEW...')
+		 * @param	array		$fieldArray: The field array of a record
+		 * @param	object		$reference: reference to parent object
+		 * @return	void
+		 */
 		function processDatamap_afterDatabaseOperations($status, $table, $id, &$fieldArray, &$reference) {
+
 			// Return if not the tt_news table or if status is not "new" or "update".
 			if ($table != 'tt_news' || !($status == 'new' || $status == 'update')) return;
 			// Return if "tx_pmkttnewstwitter_notwitter" field is enabled
@@ -63,20 +75,28 @@
 			// Return if twitter username or password is missing
 			if ($this->conf['twitterUser'] == '' || $this->conf['twitterPassword'] == '') return;
 			if (isset($fieldArray[$this->conf['postField']])) {
-				$this->uid = ($status == 'new') ?$reference->substNEWwithIDs[$id] : $id;
-				$this->status = ($status == 'new') ? 1 : 2;
+				$singleUrl = '';
+				if ($this->conf['linkBack']) {
+					$this->uid = ($status == 'new') ?$reference->substNEWwithIDs[$id] : $id;
+					$this->status = ($status == 'new') ? 1 : 2;
+					$this->tmpl = $this->init_tmpl($reference->checkValue_currentRecord['pid'],0);
+					$this->ttnewsConf = $this->tmpl->setup['plugin.']['tt_news.'];
+					$this->ttnewsCat = $this->getNewsCategory($this->uid);
+					$singleUrl = ' '.$this->createTinyUrl($this->makeSingleLink());
+				}
+				$singleUrlLen = strlen($singleUrl);
 				$msg = htmlspecialchars(strip_tags($fieldArray[$this->conf['postField']]));
-				$msg = strlen($msg) > 130 ? substr($msg, 0, 130).'...': $msg;
-				$this->twit($msg);
+				$msg = (strlen($msg)+$singleUrlLen > 136) ? substr($msg, 0, 136-$singleUrlLen).'...': $msg;
+				$this->twit($msg.$singleUrl);
 			}
 		}
-		 
+
 		/**
-		* Post data on Twitter
-		*
-		* @param string  $twitter_data: Data to post on twitter.
-		* @return void
-		*/
+		 * Post data on Twitter
+		 *
+		 * @param	string		$twitter_data: Data to post on twitter.
+		 * @return	void
+		 */
 		function twit($twitter_data) {
 			$twitter_api_url = 'http://twitter.com/statuses/update.xml';
 			$twitter_user = $this->conf['twitterUser'];
@@ -95,21 +115,98 @@
 				$reference->log('tt_news', $this->uid, $this->status, 0, 1, "pmkttnewstwitter: Something went wrong, and the tweet wasn't posted correctly.");
 			}
 		}
+
 		/**
-		* Returns config array with plugin options from Page TSConfig and Extension Config
-		*
-		* @param integer  $pageId: id of pages record
-		* @return array  Array of config options.
-		*/
+		 * Make link to tt_news singleview
+		 *
+		 * @param	void
+		 * @return	string		typolink pointing to singleview page.
+		 */
+		function makeSingleLink() {
+			//  Overwrite the singlePid from config-array with a singlePid given from the first entry in $this->categories
+			if ($this->ttnewsConf['useSPidFromCategory'] && is_array($this->ttnewsCat)) {
+				$catSPid = $this->ttnewsCat;
+			}
+			$singlePid = $catSPid['single_pid'] ? $catSPid['single_pid'] : $this->ttnewsConf['singlePid'];
+			$parameters = array(
+				'tx_ttnews' => array(
+					'tt_news' => $this->uid,
+					'backPid' => ($this->ttnewsConf['dontUseBackPid'] ? null : $this->ttnewsConf['backPid'])
+				));
+			$pagepath = tx_pagepath_api::getPagePath($singlePid, $parameters);
+			return $pagepath;
+		}
+
+		/**
+		 * Initialize TMPL object, so we can access Typoscript setups from BE.
+		 *
+		 * @param	integer		$pageId: tt_news uid.
+		 * @param	integer		$template_uid: ?? Not used.
+		 * @return	array		$tmpl: TMPL object.
+		 */
+		function init_tmpl($pageId,$template_uid=0)	{
+			//global $tmpl;
+			$tmpl = t3lib_div::makeInstance("t3lib_tsparser_ext");	// Defined global here!
+			$tmpl->tt_track = 0;	// Do not log time-performance information
+			$tmpl->init();
+			// Gets the rootLine
+			$sys_page = t3lib_div::makeInstance("t3lib_pageSelect");
+			$rootLine = $sys_page->getRootLine($pageId);
+			// This generates the constants/config + hierarchy info for the template.
+			$tmpl->runThroughTemplates($rootLine,$template_uid);
+			$tmpl->generateConfig();
+			return $tmpl;
+		}
+
+		/**
+		 * Get current tt_news category
+		 *
+		 * @param	integer		$uid: tt_news uid.
+		 * @return	array		$cat: category array.
+		 */
+		function getNewsCategory($uid) {
+			$cat = '';
+			$res = $GLOBALS['TYPO3_DB']->exec_SELECT_mm_query (
+				'tt_news_cat.*,tt_news_cat_mm.sorting AS mmsorting',
+				'tt_news',
+				'tt_news_cat_mm',
+				'tt_news_cat',
+				' AND tt_news_cat_mm.uid_local='.intval($uid).' AND tt_news_cat.deleted=0'.$this->enableCatFields
+				);
+			if ($GLOBALS['TYPO3_DB']->sql_num_rows($res)) {
+				$cat = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+			}
+			return $cat;
+		}
+
+		/**
+		 * Shorten long url by converting it using the tinyurl.com API
+		 *
+		 * @param	string		$longURL: Long URL.
+		 * @return	string		tinyurl version of long URL.
+		 */
+		function createTinyUrl($longURL) {
+			$tinyURL = @file_get_contents("http://tinyurl.com/api-create.php?url=".$longURL);
+			return $tinyURL ? $tinyURL : $longURL;
+		}
+
+		/**
+		 * Returns config array with plugin options from Page TSConfig and Extension Config
+		 *
+		 * @param	integer		$pageId: id of pages record
+		 * @return	array		Array of config options.
+		 */
 		function getConfig($pageId) {
 			$PageTSconfig = t3lib_BEfunc::getPagesTSconfig($pageId);
+			debug($PageTSconfig,'PageTSconfig');
 			$conf = $PageTSconfig['tx_pmkttnewstwitter.'];
 			$conf['twitterUser'] = htmlspecialchars($conf['twitterUser']);
 			$conf['twitterPassword'] = htmlspecialchars($conf['twitterPassword']);
 			$conf['postField'] = $conf['postField'] ? $conf['postField'] : 'bodytext';
+			$conf['linkBack'] = intval($conf['linkBack']);
 			return $conf;
 		}
-	 
+
 	}
 
 	if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/pmkttnewstwitter/class.tx_pmkttnewstwitter.php']) {
--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "pmkttnewstwitter"
 #
-# Auto generated 08-11-2009 22:37
+# Auto generated 11-11-2009 19:44
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,12 +12,12 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'PMK News Twitter',
-	'description' => 'Adds Twitter entry when a tt_news record is created or edited.',
+	'description' => 'Adds Twitter entry when a tt_news record is created or edited. Optional link back to singleView page can be added to Twitter post.',
 	'category' => 'be',
 	'author' => 'Peter Klein',
 	'author_email' => 'pmk@io.dk',
 	'shy' => '',
-	'dependencies' => 'tt_news',
+	'dependencies' => 'tt_news,pagepath',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -25,21 +25,22 @@ $EM_CONF[$_EXTKEY] = array(
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
-	'modify_tables' => 'tt_news',
+	'modify_tables' => 'tt_news,pagepath',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.2',
+	'version' => '0.0.3',
 	'constraints' => array(
 		'depends' => array(
 			'tt_news' => '2.5.0-0.0.0',
+			'pagepath' => '0.1.4-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"de31";s:10:"README.txt";s:4:"ee2d";s:29:"class.tx_pmkttnewstwitter.php";s:4:"e529";s:12:"ext_icon.gif";s:4:"d447";s:17:"ext_localconf.php";s:4:"7a3b";s:15:"ext_php_api.dat";s:4:"b347";s:14:"ext_tables.php";s:4:"0b18";s:14:"ext_tables.sql";s:4:"e3d0";s:16:"locallang_db.xml";s:4:"cef0";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"de31";s:10:"README.txt";s:4:"ee2d";s:29:"class.tx_pmkttnewstwitter.php";s:4:"b6db";s:12:"ext_icon.gif";s:4:"d447";s:17:"ext_localconf.php";s:4:"aa8f";s:15:"ext_php_api.dat";s:4:"b347";s:14:"ext_tables.php";s:4:"0b18";s:14:"ext_tables.sql";s:4:"e3d0";s:16:"locallang_db.xml";s:4:"7fad";s:14:"doc/manual.sxw";s:4:"8764";}',
 	'suggests' => array(
 	),
 );
