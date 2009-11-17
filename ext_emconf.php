8c5fe10b133c71b15a620bd4c6f466e7dff703f0
pmk65 <pmk65@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-17 17:39:59 +0000

    TER Sync
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pmkttnewstwitter/trunk@26650 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "pmkttnewstwitter"
 #
-# Auto generated 12-11-2009 14:25
+# Auto generated 17-11-2009 18:39
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.2',
+	'version' => '0.1.3',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.2.0-4.99.99',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"de31";s:10:"README.txt";s:4:"ee2d";s:29:"class.tx_pmkttnewstwitter.php";s:4:"ca1b";s:12:"ext_icon.gif";s:4:"d447";s:17:"ext_localconf.php";s:4:"aa8f";s:15:"ext_php_api.dat";s:4:"844f";s:14:"ext_tables.php";s:4:"0b18";s:14:"ext_tables.sql";s:4:"e3d0";s:16:"locallang_db.xml";s:4:"7fad";s:14:"doc/manual.sxw";s:4:"3726";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"de31";s:10:"README.txt";s:4:"ee2d";s:29:"class.tx_pmkttnewstwitter.php";s:4:"5bc9";s:12:"ext_icon.gif";s:4:"d447";s:17:"ext_localconf.php";s:4:"4372";s:15:"ext_php_api.dat";s:4:"bd83";s:14:"ext_tables.php";s:4:"0b18";s:14:"ext_tables.sql";s:4:"e3d0";s:16:"locallang_db.xml";s:4:"7fad";s:14:"doc/manual.sxw";s:4:"a810";}',
 	'suggests' => array(
 	),
 );
