2c422a89d78596f4c706843a2de93d1db4c9ceb2
pmk65 <pmk65@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-11 19:26:08 +0000

    Updated FAQ section of the manual.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pmkttnewstwitter/trunk@32129 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "pmkttnewstwitter".
 #
-# Auto generated 11-04-2010 11:56
+# Auto generated 11-04-2010 13:03
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'pagepath' => '0.1.4-0.0.0',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"de31";s:10:"README.txt";s:4:"ee2d";s:29:"class.tx_pmkttnewstwitter.php";s:4:"f1e4";s:12:"ext_icon.gif";s:4:"d447";s:17:"ext_localconf.php";s:4:"4372";s:15:"ext_php_api.dat";s:4:"bd83";s:14:"ext_tables.php";s:4:"4f01";s:14:"ext_tables.sql";s:4:"e3d0";s:17:"locallang_csh.xml";s:4:"d6d6";s:16:"locallang_db.xml";s:4:"9e31";s:14:"doc/manual.sxw";s:4:"5f46";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"de31";s:10:"README.txt";s:4:"ee2d";s:29:"class.tx_pmkttnewstwitter.php";s:4:"f1e4";s:12:"ext_icon.gif";s:4:"d447";s:17:"ext_localconf.php";s:4:"4372";s:15:"ext_php_api.dat";s:4:"bd83";s:14:"ext_tables.php";s:4:"4f01";s:14:"ext_tables.sql";s:4:"e3d0";s:17:"locallang_csh.xml";s:4:"d6d6";s:16:"locallang_db.xml";s:4:"9e31";s:14:"doc/manual.sxw";s:4:"5edc";}',
 	'suggests' => array(
 	),
 );
